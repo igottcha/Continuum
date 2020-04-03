e1c11b1410f3f011cab76866eddf77837c3eb321
Chris Gottfredson <chrisgottfredson@gmail.com>
2020-04-03 09:04:39 -0600

    added some cloudkit support to model files

--- Continuum/Models/Comment.swift
+++ Continuum/Models/Comment.swift
@@ -6,22 +6,61 @@
 //  Copyright © 2020 trevorAdcock. All rights reserved.
 //
 
-import Foundation
+import CloudKit
+
+struct CommentStrings {
+    static let recordType = "Comment"
+    fileprivate static let textKey = "text"
+    fileprivate static let timestampKey = "timestamp"
+    fileprivate static let postReferenceKey = "postReference"
+    
+}
 
 class Comment {
-    let text: String
+    var text: String
     let timestamp: Date
+    let recordID: CKRecord.ID
     weak var post: Post?
     
-    init(text: String, timestamp: Date = Date(), post: Post?) {
+    var postReference: CKRecord.Reference? {
+        guard let post = post else { return nil }
+        return CKRecord.Reference(recordID: post.recordID, action: .deleteSelf)
+    }
+    
+    init(text: String, timestamp: Date = Date(), post: Post?, recordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
         self.text = text
         self.timestamp = timestamp
         self.post = post
+        self.recordID = recordID
     }
 }
 
 extension Comment: SearchableRecord {
     func matches(searchTerm: String) -> Bool {
         text.lowercased().contains(searchTerm.lowercased())
+    }
+}
+
+extension Comment {
+    
+    convenience init?(ckRecord: CKRecord, post: Post) {
+        
+        guard let text = ckRecord[CommentStrings.textKey] as? String,
+        let timestamp = ckRecord[CommentStrings.timestampKey] as? Date else { return nil }
+     
+        self.init(text: text, timestamp: timestamp, post: post, recordID: ckRecord.recordID)
+    }
+}
+
+extension CKRecord {
+    
+    convenience init(comment: Comment) {
+        self.init(recordType: CommentStrings.recordType, recordID: comment.recordID)
+        self.setValuesForKeys([CommentStrings.textKey : comment.text,
+                               CommentStrings.timestampKey : comment.timestamp])
+        
+        if let reference = comment.postReference {
+            self.setValue(reference, forKey: CommentStrings.postReferenceKey)
+        }
     }
 }
--- Continuum/Models/Post.swift
+++ Continuum/Models/Post.swift
@@ -6,13 +6,24 @@
 //  Copyright © 2020 trevorAdcock. All rights reserved.
 //
 
-import UIKit
+import UIKit.UIImage
+import CloudKit
+
+struct PostStrings {
+    static let recordType = "Post"
+    fileprivate static let timestampKey = "timestamp"
+    fileprivate static let captionKey = "caption"
+    fileprivate static let imageAssetKey = "imageAsset"
+    fileprivate static let photoKey = "photo"
+    fileprivate static let commentsKey = "comments"
+}
 
 class Post {
     var photoData: Data?
     let timestamp: Date
-    let caption: String
+    var caption: String
     var comments: [Comment]
+    let recordID: CKRecord.ID
     
     var photo: UIImage? {
         get {
@@ -24,10 +35,25 @@ class Post {
         }
     }
     
-    init(photo: UIImage?, caption: String, timestamp: Date = Date(), comments: [Comment] = []) {
+    var imageAsset: CKAsset? {
+        get {
+            let tempDirectory = NSTemporaryDirectory()
+            let tempDirectoryURL = URL(fileURLWithPath: tempDirectory)
+            let fileURL = tempDirectoryURL.appendingPathComponent(UUID().uuidString).appendingPathExtension("jpeg")
+            do {
+                try photoData?.write(to: fileURL)
+            } catch {
+                print("Error writing to temp URL \(error) : \(error.localizedDescription)")
+            }
+            return CKAsset(fileURL: fileURL)
+        }
+    }
+    
+    init(photo: UIImage?, caption: String, timestamp: Date = Date(), comments: [Comment] = [], recordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
         self.caption = caption
         self.timestamp = timestamp
         self.comments = comments
+        self.recordID = recordID
         self.photo = photo
     }
 }
@@ -44,5 +70,37 @@ extension Post: SearchableRecord {
             }
         }
         return false
+    }
+}
+
+extension Post {
+    
+    convenience init?(ckRecord: CKRecord) {
+        var foundphoto: UIImage?
+        if let imageAsset = ckRecord[PostStrings.imageAssetKey] as? CKAsset {
+            do {
+                let data = try Data(contentsOf: imageAsset.fileURL!)
+                foundphoto = UIImage(data: data)
+            } catch {
+                print(error)
+            }
+        }
+        
+        guard let caption = ckRecord[PostStrings.captionKey] as? String,
+            let timestamp = ckRecord[PostStrings.timestampKey] as? Date else { return nil }
+       
+        self.init(photo: foundphoto, caption: caption, timestamp: timestamp, comments: [], recordID: ckRecord.recordID)
+    }
+}
+
+extension CKRecord {
+    
+    convenience init(post: Post) {
+        self.init(recordType: PostStrings.recordType, recordID: post.recordID)
+        self.setValuesForKeys([PostStrings.captionKey : post.caption,
+                               PostStrings.timestampKey : post.timestamp])
+        if post.imageAsset != nil {
+            self.setValue(post.imageAsset, forKey: PostStrings.imageAssetKey)
+        }
     }
 }
